f15de652f87910d02fa8585c91ad45e26512dfb3
Christian Fleschhut <christian@gridsingularity.com>
2018-01-25 14:12:35 +0100

    feat(numberpicker): implement increment/decrement/update amount behaviour in custom component

--- stories/NumberPicker.js
+++ stories/NumberPicker.js
@@ -1,11 +1,65 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { action } from '@storybook/addon-actions';
 import { NumberPicker } from '../build';
+
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + action.step;
+
+    case 'DECREMENT':
+      return state - action.step;
+
+    case 'UPDATE_AMOUNT':
+      return action.value;
+
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  amount: counterReducer
+});
+
+const store = createStore(rootReducer);
 
 storiesOf('NumberPicker', module)
   .add(
-    'default',
-    () => (
-      <NumberPicker />
-    ),
+    'Default',
+    withInfo()(() => (
+      <Provider store={store}>
+        <NumberPicker
+          step={1}
+        />
+      </Provider>
+    ))
+  )
+  .add(
+    'With text label',
+    withInfo()(() => (
+      <Provider store={store}>
+        <NumberPicker
+          step={1}
+          label="Einkauf"
+        />
+      </Provider>
+    ))
+  )
+  .add(
+    'With text label & currency symbol',
+    withInfo()(() => (
+      <Provider store={store}>
+        <NumberPicker
+          step={1}
+          label="Einkauf"
+          currencySymbol="€"
+        />
+      </Provider>
+    ))
   );
