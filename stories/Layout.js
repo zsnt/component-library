691a445420e1d672080fb8e32bbef782faa25a16
Kathrin Holzmann <lelith@gmx.de>
2018-03-13 12:03:08 +0100

    Setup and style results container page layout

--- stories/Layout.js
+++ stories/Layout.js
@@ -20,39 +20,46 @@ storiesOf('D3A/Layouts', module)
       ~~~
 
     `)(() => (
-      <ThemeProvider theme="light">
-        <div className="header">
-          Logo
+      <ThemeProvider theme="d3a">
+        <Layout layout="layout-wrapper">
+          <div className="header">
+            Logo
 
-        </div>
-        <Layout layout="result">
-          <div className="hero">
-            <h1>Hallo world</h1>
           </div>
-          <div className="gridLevel">
-            <h2>Grid Level</h2>
-          </div>
-          <div className="averageTable">
-            <table>
-              <tr>
-                <td>Average trade price</td>
-                <td>29,16€</td>
-              </tr>
-            </table>
-          </div>
-          <div className="gridPicture">
-            <img src={image.src} alt={image.alt} />
-          </div>
-          <div className="marketLevel">
-            <h2>Market overview</h2>
-          </div>
-          <div className="result1">
-            Result 1
-            <img src={image.src} alt={image.alt} />
-          </div>
-          <div className="result2">
-            Result 2
-            <img src={image.src} alt={image.alt} />
+          <Layout layout="results" className="content">
+            <div className="hero">
+              <h1>Hallo world</h1>
+            </div>
+            <div className="gridLevel">
+              <h2>Grid Level</h2>
+            </div>
+            <div className="averageTable">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Average trade price</td>
+                    <td>29,16€</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="gridPicture">
+              <img src={image.src} alt={image.alt} />
+            </div>
+            <div className="marketLevel">
+              <h2>Market overview</h2>
+            </div>
+            <div className="result1">
+              Result 1
+              <img src={image.src} alt={image.alt} />
+            </div>
+            <div className="result2">
+              Result 2
+              <img src={image.src} alt={image.alt} />
+            </div>
+          </Layout>
+          <div className="footer">
+            <a href="foo">About</a>
           </div>
         </Layout>
       </ThemeProvider>
