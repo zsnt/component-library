5c8718e7de65de01e146db8c7d3bfbd17c192b87
Christian Fleschhut <weltrekordhalter@gmail.com>
2018-05-14 17:01:17 +0200

    refactor(header-auth-links): extract UserStatus into own component

--- stories/Header.js
+++ stories/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import StoryRouter from 'storybook-react-router';
-import { ThemeProvider, Header } from '../lib';
+import { ThemeProvider, Header, UserStatus } from '../lib';
 import logo from './img/Logo.png';
 
 const divStyle = {
@@ -15,7 +15,7 @@ storiesOf('D3A/Organisms', module)
   .add(
     'Header',
     withInfo(`
-      Big header area called Hero. Accepts Title and Subtitle and other children.
+      Site header displaying site logo which links to home page and navigation for authenticating.
     `)(() => (
       <ThemeProvider theme="d3a">
         <div className="base" style={divStyle}>
@@ -23,8 +23,9 @@ storiesOf('D3A/Organisms', module)
             logoUrl={logo}
             logoLink="/"
             logoAlt="D3ASIM"
-            authenticated={false}
-          />
+          >
+            <UserStatus />
+          </Header>
         </div>
       </ThemeProvider>
     )),
