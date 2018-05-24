8a2976522601884059c9875489dd8e09c1723b10
Christian Fleschhut <weltrekordhalter@gmail.com>
2018-05-24 14:07:50 +0200

    feat(configuration): add text input field component

---
+++ stories/TextInputField.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { action } from '@storybook/addon-actions';
+import { ThemeProvider, TextInputField } from '../lib';
+
+const wrapperStyle = {
+  padding: '15px',
+};
+
+storiesOf('D3A/Atoms', module)
+  .add(
+    'TextInputField',
+    withInfo()(() => (
+      <ThemeProvider theme="d3a">
+        <div className="base" style={wrapperStyle}>
+          <TextInputField
+            label="Name of your Simulation"
+            htmlFor="name"
+            placeholder="Simulation Name"
+            onChange={action('onChange')}
+          />
+        </div>
+      </ThemeProvider>
+    )),
+  );
