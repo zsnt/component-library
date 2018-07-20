0ec0c314725f6813cf863c13f5fb279a14948ddd
alonavns <alona.vns@outlook.com>
2018-07-20 10:25:56 +0300

    update structure and sliding pane

--- stories/_Verbund/IntroPage.js
+++ stories/_Verbund/IntroPage.js
@@ -4,7 +4,7 @@ import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
 import {
   Header, Headline, Button,
-  Wrapper, Section, Footer,
+  Wrapper, Section, Footer, RightSlidingPane,
 } from '../../lib';
 import { ThemeDecorator } from '../Utils';
 import imageFile from '../img/placeholder-2.jpg';
@@ -51,36 +51,38 @@ storiesOf('VERBUND/Pages', module)
 
     `)(() => (
         <Wrapper layout>
-          <Header
-            logoUrl={logo}
-            logoAlt="Verbund"
-          >
-            <Headline type="verbund-subtitle">
-              Mieter Strom Modell
+          <RightSlidingPane>
+            <Header
+              logoUrl={logo}
+              logoAlt="Verbund"
+            >
+              <Headline type="verbund-subtitle">
+                Mieter Strom Modell
             </Headline>
-          </Header>
-          <Section type="intro-title">
-            Schließlich ist es<br />
-            Ihr Strom.
+            </Header>
+            <Section type="intro-title">
+              Schließlich ist es<br />
+              Ihr Strom.
             <Section type="intro-subtitle">
-              Sed posuere consectetur est at lobortis. Integer posuere erat a ante<br />
-              venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibu<br />
-              magnis dis parturient montes, nascetur ridiculus mus.
+                Sed posuere consectetur est at lobortis. Integer posuere erat a ante<br />
+                venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibu<br />
+                magnis dis parturient montes, nascetur ridiculus mus.
             </Section>
-            <div className="circles">
-              <div className="circle fill" />
-              <div className="circle" />
-              <div className="circle" />
-            </div>
-          </Section>
-          <Button
-            label="Anmelden"
-            type="intro"
-            onClick={action('clicked')}
-          />
-          <Footer type="intro">
-            <div>© VERBUND AG 2018</div>
-          </Footer>
+              <div className="circles">
+                <div className="circle fill" />
+                <div className="circle" />
+                <div className="circle" />
+              </div>
+            </Section>
+            <Button
+              label="Anmelden"
+              type="intro"
+              onClick={action('clicked')}
+            />
+            <Footer type="intro">
+              <div>© VERBUND AG 2018</div>
+            </Footer>
+          </RightSlidingPane>
         </Wrapper>
       )),
 );
