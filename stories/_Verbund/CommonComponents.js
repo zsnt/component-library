1ea5dda23521885476c95fafa187de90868b59fe
alonavns <alona.vns@outlook.com>
2018-07-20 14:34:15 +0300

    update sliding pane and panel form

--- stories/_Verbund/CommonComponents.js
+++ stories/_Verbund/CommonComponents.js
@@ -3,9 +3,13 @@ import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
 import {
-  Wrapper, Button, Footer,
+  Wrapper, Button, Footer, FormInput, FormTitle,
 } from '../../lib';
 import { ThemeDecorator } from '../Utils';
+
+import IconLogin from '../img/icon_login.svg';
+import IconEmail from '../img/icon_email.svg';
+import IconPassword from '../img/icon_password.svg';
 
 storiesOf('VERBUND/Components/Common/Button', module)
   .addDecorator(ThemeDecorator('verbund'))
@@ -31,7 +35,130 @@ storiesOf('VERBUND/Components/Common/Button', module)
           />
         </Wrapper>
       ))
+  )
+  .add(
+    'secondary',
+    withInfo(`
+        description or documentation about my component, supports markdown
+  
+        ~~~js
+        <Button
+          label="Übernehmen"
+          type="verbund-secondary"
+          onClick={action('clicked')}
+        />
+        ~~~
+  
+      `)(() => (
+        <Wrapper>
+          <Button
+            label="Übernehmen"
+            type="verbund-secondary"
+            onClick={action('clicked')}
+          />
+        </Wrapper>
+      ))
+  )
+  .add(
+    'secondary with icon',
+    withInfo(`
+        description or documentation about my component, supports markdown
+  
+        ~~~js
+        <Button
+          label="Anmelden"
+          type="verbund-secondary"
+          onClick={action('clicked')}
+        />
+        ~~~
+  
+      `)(() => (
+        <Wrapper>
+          <Button
+            label="Anmelden"
+            type="verbund-secondary"
+            icon={IconLogin}
+            onClick={action('clicked')}
+          />
+        </Wrapper>
+      ))
   );
+
+storiesOf('VERBUND/Components/Common/Input', module)
+  .addDecorator(ThemeDecorator('verbund'))
+  .add(
+    'primary',
+    withInfo(`
+        description or documentation about my component, supports markdown
+  
+        ~~~js
+        <FormInput
+          label="Input Here ..."
+          type="default"
+          onChange={action('changed')}
+        />
+        ~~~
+  
+      `)(() => (
+        <Wrapper>
+          <FormInput
+            label="Input Here ..."
+            type="default"
+            onChange={action('changed')}
+          />
+        </Wrapper>
+      ))
+  )
+  .add(
+    'primary with icon',
+    withInfo(`
+        description or documentation about my component, supports markdown
+  
+        ~~~js
+        <FormInput
+          label="E-Mail"
+          type="default"
+          onChange={action('changed')}
+          icon={IconEmail}
+        />
+        ~~~
+  
+      `)(() => (
+        <Wrapper>
+          <FormInput
+            label="E-Mail"
+            type="default"
+            onChange={action('changed')}
+            icon={IconEmail}
+          />
+          <FormInput
+            label="Password"
+            type="default"
+            onChange={action('changed')}
+            icon={IconPassword}
+          />
+        </Wrapper>
+      ))
+  );
+
+storiesOf('VERBUND/Components/Common/Title', module)
+  .addDecorator(ThemeDecorator('verbund'))
+  .add(
+    'default',
+    withInfo(`
+        description or documentation about my component, supports markdown
+  
+        ~~~js
+        <FormTitle label="Konto" />
+        ~~~
+  
+      `)(() => (
+        <Wrapper>
+          <FormTitle />
+        </Wrapper>
+      ))
+  );
+
 storiesOf('VERBUND/Components/Common', module)
   .addDecorator(ThemeDecorator('verbund'))
   .add(
--- stories/_Verbund/IntroPage.js
+++ stories/_Verbund/IntroPage.js
@@ -4,11 +4,14 @@ import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
 import {
   Header, Headline, Button,
-  Wrapper, Section, Footer, RightSlidingPane,
+  Wrapper, Section, Footer, RightSlidingPane, FormInput, FormTitle,
 } from '../../lib';
 import { ThemeDecorator } from '../Utils';
 import imageFile from '../img/placeholder-2.jpg';
 import logo from '../img/Verbund/Logo.svg';
+import IconLogin from '../img/icon_login.svg';
+import IconEmail from '../img/icon_email.svg';
+import IconPassword from '../img/icon_password.svg';
 
 storiesOf('VERBUND/Pages', module)
   .addDecorator(ThemeDecorator('verbund'))
@@ -18,40 +21,99 @@ storiesOf('VERBUND/Pages', module)
       description or documentation about my component, supports markdown
 
       ~~~js
-      <Wrapper>
-        <Header
-          logoUrl={logo}
-          logoAlt="Verbund"
-        >
-          <Headline type="verbund-subtitle">
-            Mieter Strom Modell
-          </Headline>
-        </Header>
-        <Section type="intro-title">
-          Schließlich ist es<br />
-          Ihr Strom.
-          <Section type="intro-subtitle">
-            Sed posuere consectetur est at lobortis. Integer posuere erat a ante<br />
-            venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibu<br />
-            magnis dis parturient montes, nascetur ridiculus mus.
+      <RightSlidingPane
+        pane={
+          ({onSliding}) => (
+            <div>
+              <FormTitle onClose={() => onSliding(false)} />
+              <FormInput
+                label="E-Mail"
+                type="default"
+                onChange={action('changed')}
+                icon={IconEmail}
+              />
+              <FormInput
+                label="Password"
+                type="default"
+                onChange={action('changed')}
+                icon={IconPassword}
+              />
+              <Button
+                label="Anmelden"
+                type="verbund-secondary"
+                icon={IconLogin}
+                onClick={action('clicked')}
+                style={{ marginTop: 18 }}
+              />
+            </div>
+          )
+        }
+      >
+        <Wrapper layout>
+          <Header
+            logoUrl={logo}
+            logoAlt="Verbund"
+          >
+            <Headline type="verbund-subtitle">
+              Mieter Strom Modell
+            </Headline>
+          </Header>
+          <Section type="intro-title">
+            Schließlich ist es<br />
+            Ihr Strom.
+            <Section type="intro-subtitle">
+              Sed posuere consectetur est at lobortis. Integer posuere erat a ante<br />
+              venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibu<br />
+              magnis dis parturient montes, nascetur ridiculus mus.
+            </Section>
+            <div className="circles">
+              <div className="circle fill" />
+              <div className="circle" />
+              <div className="circle" />
+            </div>
           </Section>
-          <div className="circles">
-            <div className="circle fill" />
-            <div className="circle" />
-            <div className="circle" />
-          </div>
-        </Section>
-        <Button
-          label="Anmelden"
-          type="verbund-primary"
-          onClick={action('clicked')}
-        />
-      </Wrapper>
+          <Button
+            label="Anmelden"
+            type="verbund-primary"
+            onClick={action('clicked')}
+          />
+          <Footer type="intro">
+            <div>© VERBUND AG 2018</div>
+          </Footer>
+        </Wrapper>
+      </RightSlidingPane>
       ~~~
 
     `)(() => (
-        <Wrapper layout>
-          <RightSlidingPane>
+        <RightSlidingPane
+          pane={
+            ({onSliding}) => (
+              <div>
+                <FormTitle onClose={() => onSliding(false)} />
+                <FormInput
+                  label="E-Mail"
+                  type="default"
+                  onChange={action('changed')}
+                  icon={IconEmail}
+                />
+                <FormInput
+                  label="Password"
+                  type="default"
+                  onChange={action('changed')}
+                  icon={IconPassword}
+                />
+                <Button
+                  label="Anmelden"
+                  type="verbund-secondary"
+                  icon={IconLogin}
+                  onClick={action('clicked')}
+                  style={{ marginTop: 18 }}
+                />
+              </div>
+            )
+          }
+        >
+          <Wrapper layout>
             <Header
               logoUrl={logo}
               logoAlt="Verbund"
@@ -63,11 +125,11 @@ storiesOf('VERBUND/Pages', module)
             <Section type="intro-title">
               Schließlich ist es<br />
               Ihr Strom.
-            <Section type="intro-subtitle">
-              Sed posuere consectetur est at lobortis. Integer posuere erat a ante<br />
-              venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibu<br />
-              magnis dis parturient montes, nascetur ridiculus mus.
-            </Section>
+              <Section type="intro-subtitle">
+                Sed posuere consectetur est at lobortis. Integer posuere erat a ante<br />
+                venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibu<br />
+                magnis dis parturient montes, nascetur ridiculus mus.
+              </Section>
               <div className="circles">
                 <div className="circle fill" />
                 <div className="circle" />
@@ -82,7 +144,7 @@ storiesOf('VERBUND/Pages', module)
             <Footer type="intro">
               <div>© VERBUND AG 2018</div>
             </Footer>
-          </RightSlidingPane>
-        </Wrapper>
+          </Wrapper>
+        </RightSlidingPane>
       )),
 );
