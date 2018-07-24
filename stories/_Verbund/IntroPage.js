1545d4c2073df40bc70fd92d265caa35beddad9f
breedfactory <andrea@gridsingularity.com>
2018-07-24 16:38:53 +0200

    fix(intro): try to fire the ci

--- stories/_Verbund/IntroPage.js
+++ stories/_Verbund/IntroPage.js
@@ -3,8 +3,16 @@ import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
 import {
-  Header, Headline, Button,
-  Wrapper, Section, Footer, RightSlidingPane, FormInput, FormTitle, FormLink,
+  Header,
+  Headline,
+  Button,
+  Wrapper,
+  Section,
+  Footer,
+  RightSlidingPane,
+  FormInput,
+  FormTitle,
+  FormLink,
 } from '../../lib';
 import { ThemeDecorator } from '../Utils';
 import imageFile from '../img/placeholder-2.jpg';
@@ -31,7 +39,7 @@ storiesOf('VERBUND/Pages', module)
             </Headline>
         </Header>
         <Section type="intro-title">
-          Schließlich ist es<br />
+          Schließlich ist est ccc<br />
           Ihr Strom.
             <Section type="intro-subtitle">
             Sed posuere consectetur est at lobortis. Integer posuere erat a ante<br />
@@ -56,37 +64,28 @@ storiesOf('VERBUND/Pages', module)
       ~~~
 
     `)(() => (
-        <Wrapper layout>
-          <Header
-            logoUrl={logo}
-            logoAlt="Verbund"
-          >
-            <Headline type="verbund-subtitle">
-              Mieter Strom Modell
-              </Headline>
-          </Header>
-          <Section type="intro-title">
-            Schließlich ist es<br />
-            Ihr Strom.
-              <Section type="intro-subtitle">
-              Sed posuere consectetur est at lobortis. Integer posuere erat a ante<br />
-              venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibu<br />
-              magnis dis parturient montes, nascetur ridiculus mus.
-              </Section>
-            <div className="circles">
-              <div className="circle fill" />
-              <div className="circle" />
-              <div className="circle" />
-            </div>
+      <Wrapper layout>
+        <Header logoUrl={logo} logoAlt="Verbund">
+          <Headline type="verbund-subtitle">Mieter Strom Modell</Headline>
+        </Header>
+        <Section type="intro-title">
+          Schließlich ist es<br />
+          Ihr Strom.
+          <Section type="intro-subtitle">
+            Sed posuere consectetur est at lobortis. Integer posuere erat a ante<br />
+            venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibu<br />
+            magnis dis parturient montes, nascetur ridiculus mus.
           </Section>
-          <Button
-            label="Anmelden"
-            type="verbund-primary"
-            onClick={action('clicked')}
-          />
-          <Footer type="intro">
-            <div>© VERBUND AG 2018</div>
-          </Footer>
-        </Wrapper>
-      )),
-);
+          <div className="circles">
+            <div className="circle fill" />
+            <div className="circle" />
+            <div className="circle" />
+          </div>
+        </Section>
+        <Button label="Anmelden" type="verbund-primary" onClick={action('clicked')} />
+        <Footer type="intro">
+          <div>© VERBUND AG 2018</div>
+        </Footer>
+      </Wrapper>
+    )),
+  );
