28eb3521ea9abfc34c7cee9efcdf15567aba12ba
alonavns <alona.vns@outlook.com>
2018-07-25 12:25:51 +0300

    Card, Icon and their stories

--- stories/_Verbund/CommonComponents.js
+++ stories/_Verbund/CommonComponents.js
@@ -5,6 +5,7 @@ import { action } from '@storybook/addon-actions';
 import {
   Headline,
   Wrapper, Button, Footer, FormInput, FormTitle,
+  Icon, PageCard, PageCards, Icons,
 } from '../../lib';
 import { ThemeDecorator } from '../Utils';
 
@@ -85,6 +86,108 @@ storiesOf('VERBUND/Components/Common/Button', module)
       ))
   );
 
+storiesOf('VERBUND/Components/Common/Card', module)
+  .addDecorator(ThemeDecorator('verbund'))
+  .add(
+    'Card Title',
+    withInfo(`
+        description or documentation about my component, supports markdown
+  
+        ~~~js
+        <Headline type="card-title">
+          Verbrauch
+          <div className="info">Anteil PV gegenüber Lieferanten</div>
+        </Headline>
+        ~~~
+  
+      `)(() => (
+        <Wrapper>
+          <Headline type="card-title">
+            Verbrauch
+            <div className="info">Anteil PV gegenüber Lieferanten</div>
+          </Headline>
+        </Wrapper>
+      ))
+  )
+  .add(
+    'Page Card',
+    withInfo(`
+      description or documentation about my component, supports markdown
+
+      ~~~js
+      <FormTitle label="Konto" />
+      ~~~
+
+    `)(() => (
+        <Wrapper>
+          <PageCard>
+            <Headline type="card-title">
+              Verbrauch
+            <div className="info">Anteil PV gegenüber Lieferanten</div>
+            </Headline>
+          </PageCard>
+        </Wrapper >
+      ))
+  )
+  .add(
+    'Cards',
+    withInfo(`
+      description or documentation about my component, supports markdown
+
+      ~~~js
+      <FormTitle label="Konto" />
+      ~~~
+
+    `)(() => (
+        <Wrapper>
+          <PageCards>
+            <PageCards>
+              <PageCard>
+                <Headline type="card-title">
+                  Verbrauch
+                  <div className="info">Anteil PV gegenüber Lieferanten</div>
+                </Headline>
+              </PageCard>
+              <PageCard>
+                <Headline type="card-title">
+                  Ersparnis
+                  <div className="info">Ersparnis gegenüber Lieferanten</div>
+                </Headline>
+              </PageCard>
+            </PageCards>
+            <PageCard>
+              <Headline type="card-title">
+                IhrPV-Anteil<img src={Icons.infoCircle} />
+                <div className="info">Ohne Handel verfügbare Menge an PV Energie</div>
+              </Headline>
+              <Headline type="card-title">
+                Ihr Verbrauch<img src={Icons.infoCircle} />
+                <div className="info">Ihr tatsächlicher PV-Energie Konsum</div>
+              </Headline>
+              <Headline type="card-title">
+                Ihr Kontigent<img src={Icons.infoCircle} />
+                <div className="info">Ihr aktuell verbleibendes Kontingent</div>
+              </Headline>
+            </PageCard>
+          </PageCards>
+          <PageCards>
+            <PageCard>
+              <Headline type="card-title">
+                Wochen-Übersicht
+                <div className="info">Maecenas faucibus mollis interdum.</div>
+              </Headline>
+            </PageCard>
+            <PageCard>
+              <Headline type="card-title">
+                Tages-Übersicht
+                <div className="info">Maecenas faucibus mollis interdum.</div>
+              </Headline>
+            </PageCard>
+          </PageCards>
+        </Wrapper>
+      ))
+  );
+
 storiesOf('VERBUND/Components/Common/Input', module)
   .addDecorator(ThemeDecorator('verbund'))
   .add(
@@ -139,6 +242,29 @@ storiesOf('VERBUND/Components/Common/Input', module)
             icon={IconPassword}
           />
         </Wrapper>
+      ))
+  );
+
+storiesOf('VERBUND/Components/Common', module)
+  .addDecorator(ThemeDecorator('verbund'))
+  .add(
+    'Icons',
+    withInfo(`
+        description or documentation about my component, supports markdown
+  
+        ~~~js
+        <FormTitle label="Konto" />
+        ~~~
+  
+      `)(() => (
+        <Wrapper>
+          <div style={{ display: 'flex' }}>
+            <Icon icon="email" />
+            <Icon icon="infoCircle" />
+            <Icon icon="login" />
+            <Icon icon="password" />
+          </div>
+        </Wrapper >
       ))
   );
 
