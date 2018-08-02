c9422cf0aebe26f386bc0587f4e914c32a0fc615
alonavns <alona.vns@outlook.com>
2018-08-02 16:38:32 +0300

    feat(admin-dashboard): dropdown for page 4

--- stories/_Verbund/4.AdminDashboardPage.js
+++ stories/_Verbund/4.AdminDashboardPage.js
@@ -11,7 +11,7 @@ import {
   Icon, Icons,
   PageCard, PageCards, PageCardBox, PageCardItem,
   DonutChart, BarLineChart,
-  Group,
+  Group, DropDown,
 } from '../../lib';
 import { ThemeDecorator } from '../Utils';
 
@@ -25,6 +25,77 @@ storiesOf('VERBUND/Pages', module)
       description or documentation about my component, supports markdown
 
       ~~~js
+      <Wrapper layout>
+        <Header logoUrl={Icons.verbundLogo} logoAlt="Verbund">
+          <Headline type="verbund-subtitle">Mieter Strom Modell</Headline>
+        </Header>
+        <Button
+          className="settings"
+          label="EInstellungen"
+          type="verbund-secondary"
+          icon={Icons.settings}
+          onClick={action('clicked')}
+        />
+        <Headline type="page-title">
+          Übersicht Ihrer Aktivitäten in den letzten 30 Tagen
+          <div className="info">01.01.2017 - 31.01.2017</div>
+        </Headline>
+        <PageCards>
+          <PageCard>
+            <Headline type="card-title">
+              Verbrauch
+              <div className="info">Anteil PV gegenüber Lieferanten</div>
+            </Headline>
+            <DonutChart data={[
+              { name: 'Lieferant', value: 75 },
+              { name: 'Photovoltaik', value: 25 },
+            ]} />
+          </PageCard>
+          <PageCard>
+            <Headline type="card-title">
+              Einspeisung
+              <div className="info">Rückeinspeisung in das Stromnetz</div>
+            </Headline>
+            <DonutChart data={[{
+              name: 'Einsparung', value: 45
+            }]} />
+          </PageCard>
+          <PageCard>
+            <Headline type="card-title">
+              Ersparnis
+              <div className="info">Ersparnis gegenüber Lieferanten</div>
+            </Headline>
+            <DonutChart data={[{
+              name: 'Einsparung', value: 37
+            }]} />
+          </PageCard>
+        </PageCards>
+        <PageCards>
+          <PageCard width={100}>
+            <Headline type="card-title">
+              Wochen-Übersicht
+              <div className="info">Maecenas faucibus mollis interdum.</div>
+            </Headline>
+            <BarLineChart data={BarLineChartData.hour} keys={{ line: 'preis' }} long showAllXAxis />
+          </PageCard>
+        </PageCards>
+        <PageCards>
+          <PageCard width={100}>
+            <Headline type="card-title">
+              Tages-Übersicht
+              <div className="info">Maecenas faucibus mollis interdum.</div>
+            </Headline>
+            <BarLineChart data={BarLineChartData.date} long showAllXAxis />
+          </PageCard>
+        </PageCards>
+        <Group>
+          <DropDown type={'list'} label={'Aktiver User'} value={'Administrator'} />
+          <DropDown type={'flat'} label={'Daten-Export'} value={'Download .CSV'} />
+        </Group>
+        <Footer type={'default'}>
+          <div>© VERBUND AG 2018</div>
+        </Footer>
+      </Wrapper>
       ~~~
 
     `)(() => (
@@ -92,8 +163,8 @@ storiesOf('VERBUND/Pages', module)
             </PageCard>
           </PageCards>
           <Group>
-            <div>User</div>
-            <div>User</div>
+            <DropDown type={'list'} label={'Aktiver User'} value={'Administrator'} />
+            <DropDown type={'flat'} label={'Daten-Export'} value={'Download .CSV'} />
           </Group>
           <Footer type={'default'}>
             <div>© VERBUND AG 2018</div>
